daaef8f94cfbc5f6023c704471cd48f64a979113
Ingrid <ingrid.medina@csedge.org>
2023-08-03 14:05:58 -0700

    final merge

---
+++ script.js
@@ -0,0 +1,6 @@
+function openTab() {
+    window.open("https://www.google.com/gmail/about/", "_blank");
+    }
+
+const ctoButton = document.getElementById("cto-mail")
+ctoButton.addEventListener("click", openTab)
